a40429f4e2f74ea03ac1be15fac6c07dab726faf
Andrew Thompson <thompsandr4@ecs.vuw.ac.nz>
2018-08-02 11:49:10 +1200

    Finished implementing meal time calculations
    Now correctly detects what is the current meal time (e.g. breakfast/lunch/dinner) and calculates how many hours and minutes before/after the user's selected meal time it currently is.

--- whats-for-dinner-app/src/pages/home/home.ts
+++ whats-for-dinner-app/src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { AppDataProvider } from '../../providers/app-data/app-data';
+import * as moment from "moment";
 
 @Component({
   selector: 'page-home',
@@ -8,12 +9,120 @@ import { AppDataProvider } from '../../providers/app-data/app-data';
 })
 export class HomePage {
 
+  getCapitalisedMeal() {
+    return this.appData.currentMeal.charAt(0).toUpperCase() + this.appData.currentMeal.slice(1);
+  }
+
+  convertMinutesToHoursAndMinutes(minutes) {
+    let minuteAmount = minutes;
+    if (minuteAmount < 0) {
+      minuteAmount = minuteAmount * -1;
+    }
+    let hourAmount = 0;
+    while (minuteAmount >= 60) {
+      hourAmount++;
+      minuteAmount = minuteAmount - 60;
+    }
+    if (hourAmount == 0) {
+      if (minuteAmount > 1) {
+        return minuteAmount + " minutes";
+      } else {
+        return minuteAmount + " minute";
+      }
+
+    } else if (minuteAmount == 0) {
+      if (hourAmount > 1) {
+        return hourAmount + " hours";
+      } else {
+        return hourAmount + " hour";
+      }
+
+    } else {
+      let hourUnit = "hour";
+      let minuteUnit = "minute";
+      if (hourAmount > 1) {
+        hourUnit = "hours";
+      }
+      if (minuteAmount > 1) {
+        minuteUnit = "minutes";
+      }
+
+      return hourAmount + " " + hourUnit + " " + minuteAmount + " " + minuteUnit;
+    }
+  }
+
+  getMealTimeDescription() {
+    switch (this.appData.currentMeal) {
+      case "breakfast": {
+        //if it's breakfast
+
+        //check if it's before defined breakfast time or after
+        let startTime = moment(this.appData.breakfastStart, "HH:mm").subtract(1, "minutes");
+        let endTime = moment(this.appData.breakfastTime, "HH:mm").add(1, "minutes");
+
+        let earlyBreakfast = this.appData.currentTime.isBetween(startTime,endTime);
+
+        //calculate the difference in time from the defined breakfast time
+        let timeDiff = this.appData.currentTime.diff(endTime.subtract(1, "minutes"), "minutes");
+        let timeDiffString = this.convertMinutesToHoursAndMinutes(timeDiff);
+
+        if (earlyBreakfast) { //before the defined meal time
+          return "I hope you're hungry!\n There's only " + timeDiffString + " until breakfast time!";
+        } else {
+          return "You're a bit slow this morning!\n Breakfast was " + timeDiffString + " ago!";
+        }
+
+        break;
+      }
+      case "lunch": {
+        //if it's lunch
+        //check if it's before defined lunch time or after
+        let startTime = moment(this.appData.lunchStart, "HH:mm").subtract(1, "minutes");
+        let endTime = moment(this.appData.lunchTime, "HH:mm").add(1, "minutes");
+
+        let earlyLunch = this.appData.currentTime.isBetween(startTime,endTime);
+
+        //calculate the difference in time from the defined lunch time
+        let timeDiff = this.appData.currentTime.diff(endTime, "minutes");
+        let timeDiffString = this.convertMinutesToHoursAndMinutes(timeDiff);
+
+        if (earlyLunch) { //before the defined meal time
+          return "Hang in there!\n There's only " + timeDiffString + " until lunch time!";
+        } else {
+          return "You must be having a busy day!\n Lunch was " + timeDiffString + " ago!";
+        }
+
+        break;
+      }
+      case "dinner": {
+        //if it's dinner
+        //check if it's before defined dinner time or after
+        let startTime = moment(this.appData.dinnerStart, "HH:mm").subtract(1, "minutes");
+        let endTime = moment(this.appData.dinnerTime, "HH:mm").add(1, "minutes");
+
+        let earlyDinner = this.appData.currentTime.isBetween(startTime,endTime);
+
+        //calculate the difference in time from the defined dinner time
+        let timeDiff = this.appData.currentTime.diff(endTime, "minutes");
+        let timeDiffString = this.convertMinutesToHoursAndMinutes(timeDiff);
+
+        if (earlyDinner) { //before the defined meal time
+          return "Not long now!\n There's only " + timeDiffString + " until dinner time!";
+        } else {
+          return "I bet you're starving!\n Dinner was " + timeDiffString + " ago!";
+        }
+        break;
+      }
+    }
+  }
+
   constructor(public navCtrl: NavController, public appData: AppDataProvider) {
-    this.appData.message = "Hi!";
+
 
 
   }
 
-  
+
+
 
 }
--- whats-for-dinner-app/src/providers/app-data/app-data.ts
+++ whats-for-dinner-app/src/providers/app-data/app-data.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import * as moment from 'moment';
+import * as moment from "moment";
 
 /*
   Generated class for the AppDataProvider provider.
@@ -11,19 +11,93 @@ import * as moment from 'moment';
 @Injectable()
 export class AppDataProvider {
 
-  message : any;
-
-  //breakfastStart: String;
+  breakfastStart: String;
   breakfastTime: String;
-  //breakfastEnd: String;
-  //lunchStart: String;
+  breakfastEnd: String;
+  isBreakfastTime : Boolean;
+  lunchStart: String;
   lunchTime: String;
-  //lundEnd: String;
-  //dinnerStart: String;
+  lunchEnd: String;
+  isLunchTime: Boolean;
+  dinnerStart: String;
   dinnerTime: String;
-  //dinnerend: String;
+  dinnerend: String;
+  isDinnerTime : Boolean;
+  currentMeal: String;
+  currentTime: Moment;
+
+  checkIfBreakfastTime() {
+    let currentTime = moment(this.currentTime);
+
+    //Check if it's breakfast time
+    let startTime = moment(this.breakfastStart, "HH:mm").subtract(1, "minutes");
+    let endTime = moment(this.breakfastEnd, "HH:mm").add(1, "minutes");
+
+    if ((startTime.hour() >= 12 && endTime.hour() <=12) || endTime.isBefore(startTime)) {
+      //if start time is afternoon and end time is morning, end time must be the following day
+      endTime.add(1, "days");
+      if (currentTime.hour() <= 12) {
+        currentTime.add(1, "days");
+      }
+    }
+
+    this.isBreakfastTime = currentTime.isBetween(startTime, endTime);
+  }
+
+  checkIfLunchTime() {
+    let currentTime = moment(this.currentTime);
+    //let currentTime = moment("10:30", "HH:mm");
+
+    //Check if it's lunch time
+    let startTime = moment(this.lunchStart, "HH:mm").subtract(1, "minutes");
+    let endTime = moment(this.lunchEnd, "HH:mm").add(1, "minutes");
+
+    if ((startTime.hour() >= 12 && endTime.hour() <=12) || endTime.isBefore(startTime)) {
+      //if start time is afternoon and end time is morning, end time must be the following day
+      endTime.add(1, "days");
+      if (currentTime.hour() <= 12) {
+        currentTime.add(1, "days");
+      }
+    }
+
+    this.isLunchTime = currentTime.isBetween(startTime, endTime);
+  }
+
+  checkIfDinnerTime() {
+    let currentTime = moment(this.currentTime);
+    //let currentTime = moment("10:30", "HH:mm");
+
+    //Check if it's dinner time
+    let startTime = moment(this.dinnerStart, "HH:mm").subtract(1, "minutes");
+    let endTime = moment(this.dinnerEnd, "HH:mm").add(1, "minutes");
+
+    if ((startTime.hour() >= 12 && endTime.hour() <=12) || endTime.isBefore(startTime)) {
+      //if start time is afternoon and end time is morning, end time must be the following day
+      endTime.add(1, "days");
+      if (currentTime.hour() <= 12) {
+        currentTime.add(1, "days");
+      }
+    }
+
+    this.isDinnerTime = currentTime.isBetween(startTime, endTime);
+  }
 
   determineMealTime() {
+    this.currentTime = moment();
+    //this.currentTime = moment("10:00", "HH:mm");
+    this.checkIfBreakfastTime();
+    this.checkIfLunchTime();
+    this.checkIfDinnerTime();
+
+    if (this.isDinnerTime) {
+      this.currentMeal = "dinner";
+    } else if (this.isLunchTime) {
+      this.currentMeal = "lunch";
+    }else {
+      this.currentMeal = "breakfast";
+    }
+
+    // alert("It's currently " + this.currentMeal.toUpperCase());
 
   }
 
@@ -49,17 +123,19 @@ export class AppDataProvider {
     let minsAfterDinner = (moment.duration(dinnerToBreakfast1).asMinutes()/2);
     let dinnerCutOff = moment(this.dinnerTime, "HH:mm").add(minsAfterDinner, "minutes").format("HH:mm");
 
-    alert(
-      "Breakfast Time is: " + this.breakfastTime + "\n" +
-      "Then wait " + breakfastToLunch + " until lunch" + "\n" +
-      "The cut-off time for breakfast is: " + breakfastCutOff + "\n" +
-      "Lunch Time is: " + this.lunchTime + "\n" +
-      "Then wait " + lunchToDinner + " until dinner" + "\n" +
-      "The cut-off time for lunch is: " + lunchCutOff + "\n" +
-      "Dinner Time is: " + this.dinnerTime + "\n" +
-      "Then wait " + dinnerToBreakfast1 + " until breakfast" + "\n" +
-      "The cut-off time for dinner is: " + dinnerCutOff + ". \n"
-    );
+    // alert(
+    //   "Breakfast Time is: " + this.breakfastTime + "\n" +
+    //   "Then wait " + breakfastToLunch + " until lunch" + "\n" +
+    //   "The cut-off time for breakfast is: " + breakfastCutOff + "\n" +
+    //   "Lunch Time is: " + this.lunchTime + "\n" +
+    //   "Then wait " + lunchToDinner + " until dinner" + "\n" +
+    //   "The cut-off time for lunch is: " + lunchCutOff + "\n" +
+    //   "Dinner Time is: " + this.dinnerTime + "\n" +
+    //   "Then wait " + dinnerToBreakfast1 + " until breakfast" + "\n" +
+    //   "The cut-off time for dinner is: " + dinnerCutOff + ". \n"
+    // );
+
+
 
     this.breakfastEnd = breakfastCutOff;
     this.lunchEnd = lunchCutOff;
@@ -69,23 +145,24 @@ export class AppDataProvider {
     this.lunchStart = moment(breakfastCutOff, "HH:mm").add(1, "minute").format("HH:mm");
     this.dinnerStart = moment(lunchCutOff, "HH:mm").add(1, "minute").format("HH:mm");
 
-    alert(
-      "Breakfast time is " + this.breakfastTime + ", but it is available from " + breakfastStart + " until " + breakfastCutOff + ". \n" +
-      "Lunch time is " + this.lunchTime + ",  but it is available from " + lunchStart + " until " + lunchCutOff + ". \n" +
-      "Dinner time is " + this.dinnerTime + ", but it is available from " + dinnerStart + " until " + dinnerCutOff + "."
-    );
+    // alert(
+    //   "Breakfast time is " + this.breakfastTime + ", but it is available from " + this.breakfastStart + " until " + this.breakfastEnd + ". \n" +
+    //   "Lunch time is " + this.lunchTime + ",  but it is available from " + this.lunchStart + " until " + this.lunchEnd + ". \n" +
+    //   "Dinner time is " + this.dinnerTime + ", but it is available from " + this.dinnerStart + " until " + this.dinnerEnd + "."
+    // );
 
 
   }
 
   constructor(public http: HttpClient) {
     console.log('Hello AppDataProvider Provider');
-    this.message = "default";
-    this.breakfastTime = "07:30";
+
+    this.breakfastTime = "08:00";
     this.lunchTime = "13:00";
     this.dinnerTime = "20:00";
 
     this.determineCutOffTimes();
+    this.determineMealTime();
   }
 
 }
